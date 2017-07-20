01f048a9c48bbd21b56b1dae1043dd00a830d450
Maxim Sukhochev <sukho4evmaxim@gmail.com>
2017-07-21 01:19:04 +0300

    Added player.py

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 import os
 import random
-
-CHAR_NAME = None
-CHAR_RACE = None
-CHAR_ROLE = None
+import player
 
 
 def clear():
     return os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def name_input():
-    global CHAR_NAME
-    CHAR_NAME = input('Enter your name.')
-
-
-def race_input():
-    global CHAR_RACE
-    CHAR_RACE = input('Choose your race.')
-
-
-def role_input():
-    global CHAR_ROLE
-    CHAR_ROLE = input('Choose your role.')
-
-
-class LivingCreature():
-    hp = None
-    name = None
+player.name_input()
